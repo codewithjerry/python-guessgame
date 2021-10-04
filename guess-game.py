2dd1816d2eeee0288a2f71bdee8449d3e73840c2
Jerry Chen <codewithjerry1@gmail.com>
2021-10-05 07:30:47 +0800

    完成主要猜數字邏輯(包含次數)

--- guess-game.py
+++ guess-game.py
@@ -3,14 +3,23 @@ import random
 x = random.randint(1, 50)
 print(x)
 
-while True:
-    try:
-        y = int(input('請猜一個數字(1~50):'))
-        if x == y:
-            print('猜對了!')
-        else:
-            print('猜錯了@@~')
+for i in range(5):
+    bingo = False
+    while True:
+        try:
+            y = int(input(f'第{i+1}/{5}次，請猜一個數字(1~50):'))
+            if x == y:
+                print('猜對了!')
+                bingo = True
+                break
+            else:
+                print('猜錯了@@~')
 
+            break
+        except Exception as e:
+            print('請輸入正確數字')
+
+    if bingo:
         break
-    except Exception as e:
-        print('請輸入正確數字')
+
+print(f'正確答案為:{x}')
